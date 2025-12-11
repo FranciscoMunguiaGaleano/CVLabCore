a01cb868e36ff955c37882f7bf02109cc512862b
FranciscoMunguiaGaleano <38338843+FranciscoMunguiaGaleano@users.noreply.github.com>
2025-12-11 15:27:10 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="robinhood",
+    version="1.0.0",
+    description="Library to run the CVLab Platform/",
+    author="Francisco Munguia",
+    author_email="fmg220691@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+    include_package_data=True,
+)
