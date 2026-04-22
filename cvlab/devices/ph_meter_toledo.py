39faa5863a6e33282647891edfbcc2b2de23471a
FranciscoMunguiaGaleano <fmg220691@gmail.com>
2026-04-22 15:21:27 +0100

    Added driver for toleto five easy plus pHmeter

---
+++ cvlab/devices/ph_meter_toledo.py
@@ -0,0 +1,37 @@
+import logging
+from cvlab.devices import Device
+import time
+
+logger = logging.getLogger(__name__)
+
+class ToledoPhMeter(Device):
+    """
+    Class for Toledo Ph meter. Reads Ph VAlues from the probre, temperature, date and controls READ Button.
+    """
+
+    def __init__(self, name: str, toledophmeter_url: str, servo_url: str, servo_port: str):
+        super().__init__(name=name, base_url= toledophmeter_url)
+        self.servo=Device(name="ToledoPhmeter_aux",socket_url=servo_url,port=servo_port)
+        #TODO home servo
+
+    # ------------------------------------------------------------------
+    # Button control (servo)
+    # ------------------------------------------------------------------
+    
+    def press_read_button(self):
+        logger.info("[%s] Pressing Read Button...", self.name)
+        self.servo.send_socket_command('1');time.sleep(0.7)
+        self.servo.send_socket_command('1');time.sleep(3)
+        return {"message": "[Info] Button Pressed"}
+    
+    # ------------------------------------------------------------------
+    # Ph Reading 
+    # ------------------------------------------------------------------
+    
+    def read_ph(self):
+        logger.info("[%s] Reading pH...", self.name)
+        return self.get("/read_ph")
+    
+    def status(self):
+        logger.info("[%s] Reading status...", self.name)
+        return self.get("/status")
